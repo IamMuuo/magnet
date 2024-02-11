752198d1b12e43db49617c3720dce876f10e214b
Erick <hearteric57@gmail.com>
2024-02-11 09:30:00 +0300

    fix: production url fix

--- lib/src/magnet_utils.dart
+++ lib/src/magnet_utils.dart
@@ -5,7 +5,7 @@ import 'package:magnet/magnet.dart';
 import 'package:http/http.dart' as http;
 
 extension MagnetNotificationExtension on Magnet {
-  static const String _backendUrl = "http://localhost:8000";
+  static const String _backendUrl = "http://dita.pythonanywhere.com";
   Future<List> fetchNotifications() async {
     var response = await http.get(Uri.parse("$_backendUrl/notifications/"));
 
