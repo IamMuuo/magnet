d9df341b2c7cb12887f8d473869c9029f5aadf2a
Erick <hearteric57@gmail.com>
2024-02-12 23:16:45 +0300

    chore: final target release candidate

--- lib/src/magnet_utils.dart
+++ lib/src/magnet_utils.dart
@@ -5,7 +5,7 @@ import 'package:magnet/magnet.dart';
 import 'package:http/http.dart' as http;
 
 extension MagnetNotificationExtension on Magnet {
-  static const String _backendUrl = "http://dita.pythonanywhere.com";
+  static const String _backendUrl = "http://35.212.209.181:80";
   Future<List> fetchNotifications() async {
     var response = await http.get(Uri.parse("$_backendUrl/notifications/"));
 
