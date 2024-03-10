2801b0e92e12cf76c3607bcabb69372af5e218b7
Erick <hearteric57@gmail.com>
2024-03-10 19:00:24 +0300

    chore: migrated the backend to serv00

--- lib/src/magnet_utils.dart
+++ lib/src/magnet_utils.dart
@@ -5,7 +5,7 @@ import 'package:magnet/magnet.dart';
 import 'package:http/http.dart' as http;
 
 extension MagnetNotificationExtension on Magnet {
-  static const String _backendUrl = "http://35.212.209.181:80";
+  static const String _backendUrl = "http://academia.erick.serv00.net";
   Future<List> fetchNotifications() async {
     var response = await http.get(Uri.parse("$_backendUrl/notifications/"));
 
